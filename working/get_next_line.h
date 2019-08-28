ddfd33eafb42ef2acd7341913302e2923df3f822
lthulare <48434991+lthulare@users.noreply.github.com>
2019-08-28 15:58:26 +0200

    Add files via upload
    get next line

---
+++ working/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lthulare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/15 13:52:26 by lthulare          #+#    #+#             */
+/*   Updated: 2019/07/15 13:52:33 by lthulare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <unistd.h>
+
+int					get_next_line(const int fd, char **line);
+
+#endif
