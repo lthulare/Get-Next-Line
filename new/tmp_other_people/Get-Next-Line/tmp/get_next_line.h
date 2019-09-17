9e8677834a098e21c3df1131024876a72a78de20
lthulare <48434991+lthulare@users.noreply.github.com>
2019-09-17 16:24:51 +0200

    Add files via upload
    work on

---
+++ new/tmp_other_people/Get-Next-Line/tmp/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uisrael <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/27 14:01:05 by lthulare          #+#    #+#             */
+/*   Updated: 2019/09/17 12:49:52 by gstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 9999
+# include <unistd.h>
+
+int					get_next_line(const int fd, char **line);
+
+#endif
